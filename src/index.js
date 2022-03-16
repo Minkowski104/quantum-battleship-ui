f533785beb7b7d4ca91ce18572ea3929af4f2121
Minkowski104 <muditgarg87@gmail.com>
2022-03-16 18:38:17 +0530

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+class Board1 extends React.Component{
+	render(){
+		return(
+		<div className="board1">
+			<Square/>
+			<Square/>
+			<Square/>
+			<Square/>
+		</div>
+		);
+	}
+}
+class Square extends React.Component{
+	render(){
+		return(
+		<button className="Square">H</button>
+		);
+	}
+}
+
+class Game extends React.Component{
+	render(){
+		return(
+		<div className="Game">
+			<h1 className="game_title">Quantum Battleship</h1>
+			<Board1/>
+		</div>
+		);
+	}
+}
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Game/>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
